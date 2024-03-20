ca767d53fc8f098025d78d5debaad2ea3b52dd02
Paul <paulasquith150@gmail.com>
2024-03-20 13:55:06 +0000

    fixed some of the logic issues and cleaned the html

--- script.js
+++ script.js
@@ -86,6 +86,34 @@ var upperCasedCharacters = [
   'Y',
   'Z'
 ];
+
+var generateBtn = document.querySelector('#generate');
+
+function getPasswordOptions() {
+
+  passwordLength = parseInt(window.prompt("enter a number between 8 and 128", 67)); // prompt for password legnth and complexity
+  console.log(passwordLength);
+  while (passwordLength > 7 && passwordLength < 129 || isNaN(passwordLength)) {
+    passwordLength = parseInt(window.prompt("Please add a valid entry a number between 8 and 128"));
+//use the confirm method to get the data
+var complexityLower = confirm("Password complexity. Do you want the password to contain lowercase letters? Please answer OK for yes and cancel for no.");
+var complexityUpper = confirm("Password complexity. Do you want the password to contain uppercase letters? Please answer OK for yes and cancel for no.");
+var complexityNum = confirm("Password complexity. Do you want the password to contain numbers? Please answer OK for yes and cancel for no.");
+var complexitySpec = confirm("Password complexity. Do you want the password to contain special characters? Please answer OK for yes and cancel for no.");  
+
+//show settings as console log
+console.log('Password containing Lowercase letters? ' + complexityLower)
+console.log('Uppercase letters? ' + complexityUpper)
+console.log('Numbers? ' + complexityNum)
+console.log('Special Characters? ' + complexitySpec)
+
+
+
+
+
+
+
+
 var pCharacters=(upperCasedCharacters+lowerCasedCharacters+specialCharacters+numericCharacters)
 // Array of names of arrays
 var complexArr = [
@@ -97,14 +125,49 @@ let complexityUpper="0";
 let complexityLower="0";
 let complexityNum="0";
 let complexitySpec="0";
+// create the object pCharacters then append the relevant options to it
+var pCharacters=[];
+if (complexityLower) pCharacters = pCharacters.concat(lowerCasedCharacters);
+if (complexityUpper) pCharacters = pCharacters.concat(upperCasedCharacters);
+if (complexityNum) pCharacters = pCharacters.concat(numericCharacters);
+if (complexitySpec) pCharacters = pCharacters.concat(specialCharacters);
 
-// Function to prompt user for password options
-function getPasswordOptions(pCharacters) {
+//return the object containing the variables
+return [pCharacters, passwordLength]
+}
+
+// create the function to generate the password with required options held in the object
+
+function generatePassword() {
+  var options = getPasswordOptions()
+  var pCharacters = options[0]
+  var passwordLength = options[1]
+
+  var password = ''
+  for (var i = 0; i < passwordLength; i++) {
+    var randomindex = Math.floor(Math.random() * pCharacters.length);
+    password += pCharacters[randomindex];
+  }
+  return password;
+}
+
+// Function to write it to the page
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener('click', writePassword);
+/* 
+
+// function getPasswordOptions(pCharacters) {
 
   //prompt for password length between 8 and 128 characters
-  passwordLength = parseInt(window.prompt("enter a number between 8 and 128", 67));
+//  passwordLength = parseInt(window.prompt("enter a number between 8 and 128", 67));
   console.log(passwordLength);
-  if (passwordLength > 7 && passwordLength < 129) {
+  if (passwordLength > 7 && passwordLength < 129 || isNaN(passwordLength)) {
     console.log("Thank you your password will be " + passwordLength + " characters long.");
       } else while (passwordLength > 7 !== passwordLength < 129) { //loop until matches the correct password range
    if (passwordLength<8) {
@@ -117,27 +180,27 @@ function getPasswordOptions(pCharacters) {
 }
   // need to examine NaN issues with above
   //prompt to choose complexity Lowercase, Uppercase, Numeric, Special Characters
-  var complexityLower = window.prompt("Password complexity. Do you want the password to contain lowercase letters? Please answer Y for yes and N for no.", "Y");
-  var complexityUpper = confirm("Password complexity. Do you want the password to contain uppercase letters? Please answer OK for yes and cancel for no.");
-  var complexityNum = confirm("Password complexity. Do you want the password to contain numbers? Please answer OK for yes and cancel for no.");
-  var complexitySpec = confirm("Password complexity. Do you want the password to contain special characters? Please answer OK for yes and cancel for no.");
-  console.log('Password containing Lowercase letters? ' + complexityLower)
-  console.log('Uppercase letters? ' + complexityUpper)
-  console.log('Numbers? ' + complexityNum)
-  console.log('Special Characters? ' + complexitySpec)
-  pCharacters='';
-  if (complexityLower = "Y") {
+  //var complexityLower = window.prompt("Password complexity. Do you want the password to contain lowercase letters? Please answer Y for yes and N for no.", "Y");
+  //var complexityUpper = confirm("Password complexity. Do you want the password to contain uppercase letters? Please answer OK for yes and cancel for no.");
+  //var complexityNum = confirm("Password complexity. Do you want the password to contain numbers? Please answer OK for yes and cancel for no.");
+  //var complexitySpec = confirm("Password complexity. Do you want the password to contain special characters? Please answer OK for yes and cancel for no.");
+ // console.log('Password containing Lowercase letters? ' + complexityLower)
+  //console.log('Uppercase letters? ' + complexityUpper)
+  //console.log('Numbers? ' + complexityNum)
+  //console.log('Special Characters? ' + complexitySpec)
+  // pCharacters='';
+  if (complexityLower === "Y") {
     pCharacters = (pCharacters+lowerCasedCharacters);
-  } else {} // else do nthing and move on doesn't work
-  if (complexityUpper = true) {
+  } else {} // else do nothing and move on doesn't work
+  if (complexityUpper === true) {
     pCharacters = (pCharacters+upperCasedCharacters);
-  } else if (complexityUpper = false) {} // else do nthing and move on doesn't work
-  if (complexityNum=true) {
+  } else if (complexityUpper === false) {} // else do nothing and move on doesn't work
+  if (complexityNum === true) {
     pCharacters=(pCharacters+numericCharacters);
-  } else if (complexityNum=false) {} // else do nthing and move on doesn't work
-  if (complexitySpec=true) {
+  } else if (complexityNum === false) {} // else do nothing and move on doesn't work
+  if (complexitySpec === true) {
     pCharacters=(pCharacters+specialCharacters)
-  } else if (complexitySpec){} // else do nthing and move on doesn't work
+  } else if (complexitySpec){} // else do nothing and move on doesn't work
   console.log=(pCharacters)
   return (pCharacters, passwordLength)
 }
@@ -152,7 +215,7 @@ complexityUpper=true;
   console.log(upperCasedCharacters[9])
   console.log(upperCasedCharacters[(parseInt(Math.random() * upperCasedCharacters.length))]);
   console.log(parseInt(Math.random() * 26))
-  console.log(upperCasedCharacters.length) */
+  console.log(upperCasedCharacters.length) 
  //testing if (complexityUpper === true) {
  // testing   console.log(upperCasedCharacters[(parseInt(Math.random() * upperCasedCharacters.length))]);
     // add randomising array process
@@ -162,32 +225,42 @@ complexityUpper=true;
  }
 }
 
+//* **************************************** 
 
+// Get references to the #generate element
+//var generateBtn = document.querySelector('#generate');
+
+// Function to prompt user for password options and set passwordLength
+function getPasswordOptions() {
+  // Prompt user for password options
+  // Your existing code for prompting user and setting passwordLength goes here
+
+  // Return an array with password characters and length
+  return [pCharacters, passwordLength];
+}
 
 // Function to generate password with user input
 function generatePassword() {
-  //Generate random password
-  //var pCharacters = ['f', 'D', 'r', 'Z', 'X'];
-  password='';
-  {
-    for (let i=0; i < 15; i++) {
-      const randomindex = Math.floor(Math.random() * pCharacters.length);
-      password += pCharacters[randomindex];
-      }
-    console.log(password)
-  }
-}
+  // Call getPasswordOptions to set passwordLength and pCharacters
+  var options = getPasswordOptions();
+  var pCharacters = options[0];
+  var passwordLength = options[1];
 
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+  // Generate random password
+  let password = '';
+  for (let i = 0; i < passwordLength; i++) {
+    const randomindex = Math.floor(Math.random() * pCharacters.length);
+    password += pCharacters[randomindex];
+  }
+  return password;
+}
 
 // Write password to the #password input
-function writePassword(password) {
-  var password = generatePassword();
+function writePassword() {
+  password = generatePassword();
   var passwordText = document.querySelector('#password');
-
   passwordText.value = password;
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+*/}
